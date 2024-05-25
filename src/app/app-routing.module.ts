51fcb51ba4f5b2cc26fa8188c826338c699d5c69
welnah <85287109+welnah@users.noreply.github.com>
2024-05-26 01:25:20 +0200

    this is the nav bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminListComponent } from './Admin/admin-list/admin-list.component';
 import { AdminHomeComponent } from './Admin/admin-home/admin-home.component';
+import { AdminAddComponent } from './Admin/admin-add/admin-add.component';
 
 
 const routes: Routes = [
  {path:'', pathMatch:'full', redirectTo: 'admin-home'} ,
  {path:'admin-home', component:AdminListComponent} ,
- {path:'manage-admin',component:AdminListComponent}
+ {path:'add-admin',component:AdminAddComponent}
 
 ];
 
