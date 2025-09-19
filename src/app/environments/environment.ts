b99c2c3a3a88ade6a2bff8f8ca5e6111e72641a1
Thatego Malepe <thategomalepe08@gmail.com>
2025-09-19 10:15:42 +0200

    previousUpdates
    Updates

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -3,8 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-    production: false
-  };
+  production: false,
+  apiUrl: 'https://team34finalapi20250911203340.azurewebsites.net/api' // optional, can use localhost in dev
+};
+
   
   /*
    * For easier debugging in development mode, you can import the following file
