2b59df8862cd1f357a3c7843f516cdf58f68f058
Thatego Malepe <thategomalepe08@gmail.com>
2025-09-24 21:00:45 +0200

    new
    new

--- src/app/pre-checklist.service.ts
+++ src/app/pre-checklist.service.ts
@@ -1,32 +1,21 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { PreCheckList } from './pre-check-list';
 import { environment } from './environments/environment';
-
 @Injectable({
   providedIn: 'root'
 })
 export class PreChecklistService {
-  private apiUrl = `${environment.apiUrl}/prechecklist`;
-
+ private apiUrl = `${environment.apiUrl}/prechecklist`;
   constructor(private http: HttpClient) {}
 
-  private getAuthHeaders(): HttpHeaders {
-    const token = localStorage.getItem('token') || '';
-    return new HttpHeaders({
-      'Authorization': `Bearer ${token}`,
-      'Content-Type': 'application/json'
-    });
-  }
-
   getPreChecklist(id: number): Observable<PreCheckList> {
-    const headers = this.getAuthHeaders();
-    return this.http.get<PreCheckList>(`${this.apiUrl}/${id}`, { headers });
+    return this.http.get<PreCheckList>(`${this.apiUrl}/${id}`);
   }
 
+ 
   createPreChecklist(preChecklist: any): Observable<any> {
-    const headers = this.getAuthHeaders();
-    return this.http.post<any>(this.apiUrl, preChecklist, { headers });
+    return this.http.post(this.apiUrl, preChecklist);
   }
 }
