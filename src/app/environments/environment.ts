f710f7a6a2fefdb58b4b5e312524b354b58c2dbc
Thatego Malepe <thategomalepe08@gmail.com>
2025-11-11 00:19:41 +0200

    styling
    styling

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -5,6 +5,7 @@
 export const environment = {
   production: false,
   apiUrl: 'https://team34finalapi20250911203340.azurewebsites.net/api'
+  //apiUrl: 'https://localhost:7041/api' // local API URL
 };
 
   /*
