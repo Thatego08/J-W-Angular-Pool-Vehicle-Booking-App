ffd1faa24d371b66f8d1e1e1bd3e2dbccfeab7a5
Thatego Malepe <thategomalepe08@gmail.com>
2025-12-05 08:27:22 +0200

    enviroment
    enviroment

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  //apiUrl: 'https://team34finalapi20250911203340.azurewebsites.net/api'
-  apiUrl: 'https://localhost:7041/api' // local API URL
+  apiUrl: 'https://team34finalapi-fzdsc6hyhzaga6ab.southafricanorth-01.azurewebsites.net' //apiUrl: 'https://localhost:7041/api' // local API URL'
+  //apiUrl: 'https://localhost:7041/api' // local API URL
 };
 
   /*
